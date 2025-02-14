5ec80cd04e98360d312f14742b9acaa3880f9fb8
Htay Than Aung <hta@mit.com.mm>
2025-02-14 12:55:19 +0630

    I Love You So Much Bae

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // title = 'Do-You-Love-Me-Sweet-Heart';
-  // loveMessage = '';
-
-  // showLove() {
-  //   this.loveMessage = 'I knew it! Love you too! 💖🥰';
-  // }
-
-  // moveButton(button: HTMLButtonElement) {
-  //   const maxX = window.innerWidth - button.offsetWidth;
-  // const maxY = window.innerHeight - button.offsetHeight;
-
-  // const x = Math.random() * maxX;
-  // const y = Math.random() * maxY;
-
-  // button.style.position = 'absolute';
-  // button.style.left = `${x}px`;
-  // button.style.top = `${y}px`;
-  // }
   loveMessage: string = "";
   showLoveMessage: boolean = false;
   showPoem: boolean = false;
   showNoButton: boolean = true;
+  showMemories: boolean = false;
   displayedLines: string[] = [];
-  noButtonClickCount = 0; // Track how many times "No" button is clicked
+  noButtonClickCount = 0;
 
   poemLines: string[] = [
     "i'll be everything you need me to be.",
@@ -45,6 +28,12 @@ export class AppComponent {
     "i'll be your saviour and your hero.",
     "i'll be the walls you need to keep the building stable.",
     "i'll be everything you need me to be."
+  ];
+
+  memoryImages = [
+    "assets/memory1.jpg",
+    "assets/memory2.jpg",
+    "assets/memory3.jpg"
   ];
 
   constructor(private renderer: Renderer2) {}
@@ -66,6 +55,10 @@ export class AppComponent {
         this.displayedLines.push(line);
       }, index * 2000);
     });
+
+    setTimeout(() => {
+      this.showMemories = true; 
+    }, this.poemLines.length * 2000);
   }
 
   moveNoButton(event: Event) {
@@ -79,12 +72,12 @@ export class AppComponent {
     const noButton = event.target as HTMLElement;
 
     if (this.noButtonClickCount === 0) {
-      this.renderer.addClass(noButton, 'cracked'); // First click → Crack
+      this.renderer.addClass(noButton, 'cracked');
       this.noButtonClickCount++;
     } else if (this.noButtonClickCount === 1) {
-      this.renderer.addClass(noButton, 'shattered'); // Second click → Shatter
+      this.renderer.addClass(noButton, 'shattered');
       setTimeout(() => {
-        this.showNoButton = false; // Remove button after shattering
+        this.showNoButton = false;
       }, 500);
     }
   }
@@ -93,7 +86,7 @@ export class AppComponent {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
     const newX = Math.random() * (screenWidth - 100);
-    const newY = Math.random() * (screenHeight - 200);
+    const newY = Math.random() * (screenHeight - 100);
 
     button.style.position = "absolute";
     button.style.left = `${newX}px`;
