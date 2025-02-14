cb52e34448eb589d3308f90313edcacbc5fd7091
Htay Than Aung <hta@mit.com.mm>
2025-02-14 11:33:54 +0630

    I Love You

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Do-You-Love-Me-Sweet-Heart';
+  loveMessage = '';
+
+  showLove() {
+    this.loveMessage = 'I knew it! Love you too! 💖🥰';
+  }
+
+  moveButton(button: HTMLButtonElement) {
+    const maxX = window.innerWidth - button.offsetWidth;
+  const maxY = window.innerHeight - button.offsetHeight;
+
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+
+  button.style.position = 'absolute'; // Ensure it moves freely
+  button.style.left = `${x}px`;
+  button.style.top = `${y}px`;
+  }
 }
